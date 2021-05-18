70111bbc89369117e8b5a48bd5f107783472f325
applecat <3075202650@qq.com>
2021-05-18 09:54:55 +0800

    add scratchapad

--- config.h
+++ config.h
@@ -65,6 +65,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+
 /* mycommands */
 static const char *volup[] = { "amixer", "-qM", "set", "Master", "2%+", "umute", NULL };
 static const char *voldown[] = { "amixer", "-qM", "set", "Master", "2%-", "umute", NULL };   
@@ -84,6 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_w,      spawn,          {.v = screenshots } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
